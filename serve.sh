261bdd8a113ec37c2804a014d707603fe86036a2
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2025-06-04 02:45:58 +0300

    initial boilerplate

---
+++ serve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -m http.server 19001 --bind 127.0.0.1 -d site/
---
+++ site/main.ts
@@ -0,0 +1,5 @@
+const phrase = "all you need in this life is ignorance and confidence, and then success is sure"
+
+export function main(): void {
+  console.log("Hello from TypeScript")
+}
